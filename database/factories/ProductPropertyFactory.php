bc1cb2a5499977ef5730aa5755eb7b3b70f2a600
Sergey <1@php-cat.com>
2024-01-09 03:35:30 +0500

    создал модельки миграции связки

---
+++ database/factories/ProductPropertyFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductPropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2024_01_08_221952_create_product_product_property_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductProductPropertyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_product_property', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('product_property_id');
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('product_property_id')->references('id')->on('product_properties')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_product_property');
+    }
+}
